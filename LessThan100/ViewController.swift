ca6d3c8b41500d0d409e4a08b386c5ac249fe854
Daniel Washington <danielvertigo@hotmail.com>
2021-06-16 12:41:41 -0300

    Initial Commit

---
+++ LessThan100/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessThan100
+//
+//  Created by Daniel Washington Ignacio on 16/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
